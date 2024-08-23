f197c655d6f7bc5a2589ea9bdb4d6d0f205b1c4f
codetil <dakshapatil960@gmail.com>
2024-08-23 14:46:17 +0530

    ..

--- Task_no_3.java
+++ Task_no_3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Task_no_3 {
     public static void main(String[] args) {
-        ATM atm = new ATM(new BankAccount(10000)); // Initial balance of 10000
+        ATM atm = new ATM(new BankAccount(50000)); // Initial balance of 50000
         atm.start();
     }
 }
